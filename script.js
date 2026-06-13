19aa8783e3a388d625611e52b6f85319216cbcc5
Débora <dssl1@aluno.ifal.edu.br>
2026-06-13 10:44:24 -0300

    Alterando no VS Code

--- script.js
+++ script.js
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
-console.log("Meu nome é Débora")
-=======
-console.log("Meu nome é Débora")
->>>>>>> 3c1cd01f7ae79e0820fac0047bfce6315e94250b
+console.log("Meu nome é Débora e pronto")
+
